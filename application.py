61405f77509471111e679ff5dfdd4798072cf04c
melj <m3lissa.johnson@hotmail.com>
2020-07-13 17:05:16 -0700

    Changed the css, added bootstrap library and some icons. Fixed some of the methods and routes. Sign up works now, greeting the user, saving reviews, logout, ratings with books.

--- application.py
+++ application.py
@@ -32,40 +32,73 @@ def index():
 		user = db.execute("SELECT * FROM users WHERE username=:name", {"name": name}).fetchone()
 
 		if user is None or user.password != pword:
-			return render_template("error.html", message="Access denied. Wrong credentials.")
+			return render_template("login.html", error="Access denied. Wrong credentials.")
 		else:
-			session['curr_user'] = name;
+			session['curr_user'] = user.username
 
-	books = db.execute("SELECT * FROM books").fetchall()
-	
-	if books is None:
-		return render_template("error.html", "Could not get books. Please try again")
-	return render_template("index.html", books=books)
+	books = db.execute("SELECT * FROM books LIMIT 10").fetchall()
+
+	if 'curr_user' not in session:
+		user = None
+	else:
+		user = session['curr_user']
+
+	return render_template("index.html", books=books, user=user)
 
 @app.route("/book/<string:isbn>", methods=["GET", "POST"])
 def book(isbn):
-	if request.method == "POST":
-		if 'curr_user' not in session:
-			return render_template("error.html", message='Please Login or Sign up to review this book')			
-		else:
-			user_id = session['curr_user'].id						
-			content = request.form.get("review")
-			rating = request.form.get("rating")
-			db.execute("INSERT INTO reviews (rating, content, user_id, book_id) VALUES (:rating, :content, :user_id, :book_id)",
-                    {"rating": rating, "content": content, "user_id": user_id, "book_id": book.id})
-			db.commit()
-
 	book = db.execute("SELECT * FROM books WHERE isbn = :isbn", {"isbn": isbn}).fetchone()
 	if book is None:
 		return render_template("error.html", message="No book was found for this ISBN code")
 
+	if request.method == "POST":
+		if 'curr_user' not in session:
+			return render_template("error.html", message='Please Login or Sign up to review this book')			
+		else:
+			user = db.execute("SELECT * FROM users WHERE username=:name", {"name": session["curr_user"]}).fetchone()
+			content = request.form.get("review")
+			rating = request.form.get("rating")
+			db.execute("INSERT INTO reviews (rating, content, user_id, book_id) VALUES (:rating, :content, :user_id, :book_id)",
+                    {"rating": rating, "content": content, "user_id": user.username, "book_id": book.id})
+			db.commit()
+
 	reviews = db.execute("SELECT * FROM reviews WHERE book_id = :book_id", {"book_id": book.id}).fetchall()
 	session['curr_book'] = book	
-	return render_template("book.html", book=book, reviews=reviews)
+
+	if 'curr_user' not in session:
+		user = None
+	else:
+		user = session['curr_user']
+
+	return render_template("book.html", book=book, reviews=reviews, user=user)
 
 @app.route("/logout")
 def logout():
 	session.pop('curr_user')
 	return redirect(url_for("login"))
+
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+	if request.method == "GET":
+		return render_template("signup.html")
+
+	name = request.form.get('username')
+	password = request.form.get('password')
+	confirm_password = request.form.get('confirm-password')
+
+	if password != confirm_password:
+		return render_template("signup.html", error="Password's don't match")
+
+	user = db.execute("SELECT * FROM users WHERE username=:name", {"name": name}).fetchone()
+
+	if user is None:
+		# create a user and log them in
+		db.execute("INSERT INTO users (username, password) VALUES (:username, :password)",
+                    {"username": name, "password": password })
+		db.commit()
+		session['curr_user'] = name
+		return redirect(url_for("index"))
+	else:
+		return render_template("signup.html", error="Username " + name +  " already exists")
 
 
