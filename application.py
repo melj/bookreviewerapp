47960f3173c65bcfbb61b8489c003c58a86405f7
melj <m3lissa.johnson@hotmail.com>
2020-07-17 20:43:37 -0700

    Did some refactoring. Added documentation to application.py git status

--- application.py
+++ application.py
@@ -20,74 +20,54 @@ Session(app)
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
-@app.route("/")
+
+# Log in a user Route
+@app.route("/", methods=["GET", "POST"])
 def login():
-	return render_template("login.html")
+	# GET request, just render template
+	if request.method == "GET":
+		return render_template("login.html")
 
-@app.route("/index", methods=["GET", "POST"])
-def index():
-	if request.method == "POST":
-		name = request.form.get('username')
-		pword = request.form.get('password')
-		user = db.execute("SELECT * FROM users WHERE username=:name", {"name": name}).fetchone()
+	# POST request, get form data
+	name = request.form.get('username')
+	pword = request.form.get('password')
 
-		if user is None or user.password != pword:
-			return render_template("login.html", error="Access denied. Wrong credentials.")
-		else:
-			session['curr_user'] = user.username
+	# validate empty fields, render error if so
+	if len(name) < 1 or len(pword) < 1:
+		return render_template("login.html", error="Access denied. Fill in the fields.")
 
-	books = db.execute("SELECT * FROM books LIMIT 10").fetchall()
+	# retrieve user from database
+	user = db.execute("SELECT * FROM users WHERE username=:name", {"name": name}).fetchone()
 
-	if 'curr_user' not in session:
-		user = None
-	else:
-		user = session['curr_user']
+	# validate user exists and password, render error if so
+	if user is None or user.password != pword:
+		return render_template("login.html", error="Access denied. Wrong credentials.")
+	
+	# All good, update session and redirect to index page
+	session['curr_user'] = user.username
+	return redirect(url_for("index"))
 
-	return render_template("index.html", books=books, user=user)
-
-@app.route("/book/<string:isbn>", methods=["GET", "POST"])
-def book(isbn):
-	book = db.execute("SELECT * FROM books WHERE isbn = :isbn", {"isbn": isbn}).fetchone()
-	if book is None:
-		return render_template("error.html", message="No book was found for this ISBN code")
-
-	if request.method == "POST":
-		if 'curr_user' not in session:
-			return render_template("error.html", message='Please Login or Sign up to review this book')			
-		else:			
-			content = request.form.get("review")
-			rating = request.form.get("rating")
-			db.execute("INSERT INTO reviews (rating, content, user_id, book_id) VALUES (:rating, :content, :user_id, :book_id)",
-                    {"rating": rating, "content": content, "user_id": session['curr_user'], "book_id": book.id})
-			db.commit()
-
-	reviews = db.execute("SELECT * FROM reviews WHERE book_id = :book_id", {"book_id": book.id}).fetchall()
-	session['curr_book'] = book	
-
-	if 'curr_user' not in session:
-		user = None
-	else:
-		user = session['curr_user']
-
-	return render_template("book.html", book=book, reviews=reviews, user=user)
-
-@app.route("/logout")
-def logout():
-	session.pop('curr_user')
-	return redirect(url_for("login"))
-
+# Get the sign up page, Or Create a new user Route
 @app.route("/signup", methods=["GET", "POST"])
 def signup():
+	# GET request, just render template
 	if request.method == "GET":
 		return render_template("signup.html")
 
+	# POST request, get form data
 	name = request.form.get('username')
 	password = request.form.get('password')
 	confirm_password = request.form.get('confirm-password')
 
-	if password != confirm_password:
-		return render_template("signup.html", error="Password's don't match")
+	# validate empty fields, render error if so
+	if len(name) < 1 or len(password) < 1:
+		return render_template("signup.html", error="Ensure all fields are filled out.")
 
+	# validate password fields, render error if so
+	if password != confirm_password:
+		return render_template("signup.html", error="Password's don't match.")
+
+	# validate user does not already exist
 	user = db.execute("SELECT * FROM users WHERE username=:name", {"name": name}).fetchone()
 
 	if user is None:
@@ -98,6 +78,54 @@ def signup():
 		session['curr_user'] = name
 		return redirect(url_for("index"))
 	else:
-		return render_template("signup.html", error="Username " + name +  " already exists")
+		# the user name already exists, load page with error message
+		return render_template("signup.html", error="The username '" + name +  "'' already exists")
 
+# Main page displays all the books
+@app.route("/index", methods=["GET", "POST"])
+def index():
+	# Retrieve 10 books from the db
+	books = db.execute("SELECT * FROM books LIMIT 10").fetchall()
+
+	# if 'curr_user' not in session:
+	# 	user = None
+	# else:
+	# 	user = 
+
+	return render_template("index.html", books=books, user=session['curr_user'])
+
+# Get a specific book's details and reviews
+# User can also create a review here
+@app.route("/book/<string:isbn>", methods=["GET", "POST"])
+def book(isbn):
+	book = db.execute("SELECT * FROM books WHERE isbn = :isbn", {"isbn": isbn}).fetchone()
+	if book is None:
+		return render_template("error.html", message="No book was found for this ISBN code")
+
+	if request.method == "POST":
+		if 'curr_user' not in session:
+			return render_template("error.html", message='Please Login or Sign up to review this book')						
+
+		content = request.form.get("review")
+		rating = request.form.get("rating")
+		db.execute("INSERT INTO reviews (rating, content, user_id, book_id) VALUES (:rating, :content, :user_id, :book_id)",
+                {"rating": rating, "content": content, "user_id": session['curr_user'], "book_id": book.id})
+		db.commit()
+
+	reviews = db.execute("SELECT * FROM reviews WHERE book_id = :book_id", {"book_id": book.id}).fetchall()
+	session['curr_book'] = book	
+
+	# if 'curr_user' not in session:
+	# 	user = None
+	# else:
+	# 	user = 
+
+	return render_template("book.html", book=book, reviews=reviews, user=session['curr_user'])
+
+# Sign the user out Route
+@app.route("/logout")
+def logout():
+	# Pop the user from our session and go back to login
+	session.pop('curr_user')
+	return redirect(url_for("login"))
 
